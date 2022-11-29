1b517dd30a6325e144c27e6cd33b4ac1dffaf8b3
RagunathA_49 <ragunathgithub@gmail.com>
2022-11-29 19:02:12 +0000

    Personal detail

---
+++ script.js
@@ -0,0 +1,9 @@
+// 
+
+
+var typed = new Typed(".typing",{
+    strings:["","Web Designer","Web Developer","Programmer","Security Engineer"],
+    typedSpeed:80,
+    BackSpeed:50,
+    loop:true
+})
